a161f61c4fc3dabec8585a6ba839abe2a7a21afe
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2019-03-21 15:28:50 +0100

    versions

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -56,7 +56,7 @@ test_requires:
     Test::More:          0
     Test::NoWarnings:    0
 test_recommends:
-    Test::More:          1.302141
+    Test::More:          1.302162
 resources:
     license:             http://dev.perl.org/licenses/
     repository:          https://github.com/Tux/Text-OutputFilter
